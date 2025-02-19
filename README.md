6810e7299df5faba1c28a65eebb7f88f4c961edb
Christoph Welcz <christoph.welcz@codecentric.de>
2025-02-19 22:52:23 +0100

    🚚 move demo files to subfolder

--- README.md
+++
@@ -1,5 +0,0 @@
-# TDD in Frontend
-
-Simple demo code that was completely test-driven
-using [React Testing Library](https://testing-library.com/docs/react-testing-library/intro/) to implement a simple Todo
-App.
--- setup-tests.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom/vitest'
-import { afterEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
-
-afterEach(() => {
-  // unlike jest, the dom won't be cleaned after each test run. so we have to trigger it by hand
-  cleanup()
-})
