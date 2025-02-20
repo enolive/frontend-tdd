cf29441542e4509fe0ebf29b191a9a4f3f678de1
Christoph Welcz <christoph.welcz@codecentric.de>
2025-02-20 12:57:02 +0100

    ⬆️ use latest slidev theme version

--- slides/components/MyQr.vue
+++ slides/components/MyQr.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import QrcodeVue from 'qrcode.vue'
-</script>
-
 <template>
-    <qr-code value="https://github.com/enolive/frontend-tdd" />
+  <qr-code value="https://github.com/enolive/frontend-tdd"/>
 </template>
